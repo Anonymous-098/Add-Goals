66a6fe1223fcaf5bd0ac5e63e5cc12bd0f70888f
Rishabh <rishusingal568@gmail.com>
2022-04-18 00:15:39 +0530

    Added dynamic inline styles

--- src/components/GoalForm.js
+++ src/components/GoalForm.js
@@ -1,33 +1,49 @@
 import React, { useState } from "react";
 import "./GoalForm.css";
 
-const GoalForm = (props) =>{
-    const [enteredGoal,setEnteredGoal] = useState('');
- 
-    const submitHandler = (e) =>{
-        e.preventDefault();
+const GoalForm = (props) => {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
-        const newGoal = {
-            id:Math.random().toString(),
-            desc:enteredGoal,
-        }
-        props.addNewGoal(newGoal);
-        setEnteredGoal('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const newGoal = {
+      id: Math.random().toString(),
+      desc: enteredGoal,
+    };
+    if (enteredGoal.trim().length === 0) {
+      setIsValid(false);
+      return;
     }
+    props.addNewGoal(newGoal);
+    setEnteredGoal("");
+    setIsValid(true);
+  };
 
-    const inputHandler = (e) =>{
-        setEnteredGoal(e.target.value);
+  const inputHandler = (e) => {
+    if (e.target.value.trim().length > 0) {
+      setIsValid(true);
     }
+    setEnteredGoal(e.target.value);
+  };
 
-    return(
-        <div className="form-controller">
-            <form className="form" onSubmit={submitHandler}>
-                <label>Course Goal</label>
-                <input type="text" onChange={inputHandler} value={enteredGoal}></input>
-                <button type="submit">Add Goal</button>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div className="form-controller">
+      <form className="form" onSubmit={submitHandler}>
+        <label style={{ color: !isValid ? "red" : "black" }}>Course Goal</label>
+        <input
+          type="text"
+          onChange={inputHandler}
+          value={enteredGoal}
+          style={{
+            borderColor: !isValid ? "red" : "black",
+            backgroundColor: !isValid ? "salmon" : "transparent",
+          }}
+        ></input>
+        <button type="submit">Add Goal</button>
+      </form>
+    </div>
+  );
+};
 
 export default GoalForm;
